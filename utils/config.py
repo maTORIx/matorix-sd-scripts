9f14adc236ecdb117816ee84c584ed05f196a0e1
matorix <matorixmatrix@gmail.com>
2023-03-27 18:09:53 +0900

    bug fix

--- utils/config.py
+++ utils/config.py
@@ -7,12 +7,12 @@ CONFIG_PATH = os.path.join(BASE_DIR, "config.json")
 def load_config():
     with open(CONFIG_PATH, "r") as f:
         data = json.load(f)
-    path_check_keys = ["sd_scripts_path", "deepdanbooru_project_path"]
-    for key in path_check_keys:
-        os.chdir(BASE_DIR)
-        data[key] = os.path.abspath(data[key])
-        if not os.path.isdir(data[key]):
-            raise ValueError(f"{key} is not a valid directory, please check out the README.md")
+    os.chdir(BASE_DIR)
+    
+    data["sd_scripts_path"] = os.path.abspath(data["sd_scripts_path"])
+    if not os.path.isdir(data["sd_scripts_path"]):
+        raise ValueError("sd_scripts_path is not a valid directory, please check out the README.md")
+
     data["output_dir"] = os.path.abspath(data["output_dir"])
     if not os.path.isdir(data["output_dir"]):
         os.makedirs(data["output_dir"])
