4ef15559790ad292ecbf09b5fa204cc3dbf3c16a
matorix <matorixmatrix@gmail.com>
2023-03-28 13:52:27 +0900

    bugfix

--- setup.bat
+++ setup.bat
@@ -3,8 +3,9 @@ cd /d %~dp0
 @REM Download Waifu Diffusion repo, model and tags
 if not exist "SW_CV_ModelZoo" git clone https://github.com/SmilingWolf/SW-CV-ModelZoo -o SW_CV_ModelZoo
 if not exist "models" mkdir models
-if not exist "models\wd14" mkdir models\wd14
-cd models\wd14
+cd models
+if not exist "wd14" mkdir wd14
+cd wd14
 if not exist "model.onnx" (
     powershell -Command "Invoke-WebRequest -Uri https://huggingface.co/SmilingWolf/wd-v1-4-swinv2-tagger-v2/resolve/main/model.onnx -OutFile model.onnx -UseBasicParsing -Verbose"
 )
