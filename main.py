f89fe5d452264c9558cbec76c04b8c74f3b136e2
matorix <matorixmatrix@gmail.com>
2023-08-04 14:39:26 +0900

    support sdxl

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from tkinter import filedialog
 from utils.config import CONFIG, BASE_DIR
 from utils import tagimg, generate_image, cache
 import jinja2
+import typing
 
 root = tk.Tk()
 root.title("matroix-sd-scripts")
@@ -16,208 +17,185 @@ root.configure(background="white")
 
 # style
 style = ttk.Style()
-
-# button width auto height 20px border black no shadow
 style.configure("TButton", width=20, height=20, borderwidth=1, relief="flat", background="white", foreground="black", font="Consolas 11", anchor="center")
-
-# option menu width 20px border black no shadow
-style.configure("TOptionMenu", width=20, borderwidth=1, relief="flat", background="white", foreground="black", font="Consolas 11", anchor="left")
+style.configure("TCombobox", width=20, borderwidth=1, relief="flat", background="white", foreground="black", font="Consolas 11", anchor="left")
 
 CACHE = cache.load_cache()
 default_args = CONFIG["training_default_args"]
+row = -1
+tk_values = {}
+
+def next_row():
+    global row
+    row += 1
+    return row
+
+def add_checkbox(text: str):
+    global row
+    row += 1
+    label = tk.Label(root, text=text)
+    label.grid(row=row, column=0, sticky="w")
+    var = tk.BooleanVar(value=CACHE.get(text, False))
+    checkbox = tk.Checkbutton(root, variable=var)
+    checkbox.grid(row=row, column=1, sticky="w")
+    tk_values[text] = var
+    return var
+
+def add_option_menu(text: str, options: typing.List[str]):
+    global row
+    row += 1
+    options = [str(x) for x in options]
+    label = tk.Label(root, text=text)
+    label.grid(row=row, column=0, sticky="w")
+    var = tk.StringVar(value=CACHE.get(text, options[0]))
+    combobox = ttk.Combobox(root, textvariable=var, values=options, style="TCombobox")
+    combobox["values"] = options
+    combobox.grid(row=row, column=1, sticky="w")
+    # option_menu = ttk.OptionMenu(root, var, var.get(), *options, style="TOptionMenu")
+    # option_menu.grid(row=row, column=1, sticky="w")
+    tk_values[text] = var
+    return var
+
+def add_path_entry(text: str, dir=False):
+    global row
+    row += 1
+    label = tk.Label(root, text=text)
+    label.grid(row=row, column=0, sticky="w")
+    var = tk.StringVar(value=CACHE.get(text, ""))
+    entry = tk.Entry(root, textvariable=var)
+    entry.grid(row=row, column=1, sticky="w")
+    if dir:
+        button = ttk.Button(root, text="Browse", command=lambda: var.set(filedialog.askdirectory()), style="TButton")
+    else:
+        button = ttk.Button(root, text="Browse", command=lambda: var.set(filedialog.askopenfilename()), style="TButton")
+    button.grid(row=row, column=2)
+    tk_values[text] = var
+    return var
+
+def add_entry(text: str, varType="string", initialValue=None):
+    global row
+    row += 1
+    label = tk.Label(root, text=text)
+    label.grid(row=row, column=0, sticky="w")
+    if varType == "int":
+        if initialValue is None:
+            initialValue = 0
+        var = tk.IntVar(value=CACHE.get(text, initialValue))
+    elif varType == "double":
+        if initialValue is None:
+            initialValue = 0.0
+        print(initialValue)
+        var = tk.DoubleVar(value=CACHE.get(text, initialValue))
+    elif varType == "string":
+        if initialValue is None:
+            initialValue = ""
+        var = tk.StringVar(value=CACHE.get(text, initialValue))
+    else:
+        raise ValueError(f"Invalid varType: {varType}")
+    entry = tk.Entry(root, textvariable=var)
+    entry.grid(row=row, column=1, sticky="w")
+    tk_values[text] = var
+    return var
+
+def add_label(text: str):
+    global row
+    row += 1
+    label = tk.Label(root, text=text)
+    label.grid(row=row, column=0, sticky="w")
 
 # model name
-model_output_name_label = tk.Label(root, text="Model Name")
-model_output_name_label.grid(row=0, column=0, sticky="w")
-model_output_name_var = tk.StringVar(value="")
-model_output_name_entry = tk.Entry(root, textvariable=model_output_name_var)
-model_output_name_entry.grid(row=0, column=1)
-
-# src directory
-src_label = tk.Label(root, text="Source Directory")
-src_label.grid(row=1, column=0, sticky="w")
-src_entry = tk.Entry(root)
-src_entry.grid(row=1, column=1)
-src_button = ttk.Button(root, text="Browse", command=lambda: src_entry.insert(0, filedialog.askdirectory()), style="TButton")
-src_button.grid(row=1, column=2)
-
-# stable diffusion directory
-sd_path_label = tk.Label(root, text="StableDiffusion model path")
-sd_path_label.grid(row=2, column=0, sticky="w")
-sd_path_var = tk.StringVar(value=CACHE.get("sd_path", ""))
-sd_path_entry = tk.Entry(root, textvariable=sd_path_var)
-sd_path_entry.grid(row=2, column=1)
-sd_path_button = ttk.Button(root, text="Browse", command=lambda: sd_path_entry.insert(0, filedialog.askopenfilename()), style="TButton")
-sd_path_button.grid(row=2, column=2)
-
-# training options
-train_label = tk.Label(root, text="Training Options")
-train_label.grid(row=3, column=0, sticky="w")
-
-steps_label = tk.Label(root, text="Steps")
-steps_label.grid(row=4, column=0, sticky="w")
-steps_var = tk.IntVar(value=CACHE.get("steps", default_args["steps"]))
-steps_entry = tk.Entry(root, textvariable=steps_var)
-steps_entry.grid(row=4, column=1)
-
-batch_label = tk.Label(root, text="Batch Size")
-batch_label.grid(row=5, column=0, sticky="w")
-batch_size_var = tk.IntVar(value=CACHE.get("batch_size", default_args["batch_size"]))
-batch_entry = tk.Entry(root, textvariable=batch_size_var)
-batch_entry.grid(row=5, column=1)
-
-learning_rate_label = tk.Label(root, text="Learning Rate")
-learning_rate_label.grid(row=6, column=0, sticky="w")
-learning_rate_var = tk.DoubleVar(value=CACHE.get("learning_rate", default_args["learning_rate"]))
-learning_rate_entry = tk.Entry(root, textvariable=learning_rate_var)
-learning_rate_entry.grid(row=6, column=1)
-
-enable_bucket_label = tk.Label(root, text="Enable image ratio bucket")
-enable_bucket_label.grid(row=7, column=0, sticky="w")
-enable_bucket_var = tk.BooleanVar(value=True)
-enable_bucket_checkbox = tk.Checkbutton(root, variable=enable_bucket_var)
-enable_bucket_checkbox.grid(row=7, column=1, sticky="w")
-
-xformers_label = tk.Label(root, text="Xformers")
-xformers_label.grid(row=8, column=0, sticky="w")
-xformers_var = tk.BooleanVar(value=CACHE.get("xformers", default_args["xformers"]))
-xformers_checkbox = tk.Checkbutton(root, variable=xformers_var)
-xformers_checkbox.grid(row=8, column=1, sticky="w")
-
-# network
-network_label = tk.Label(root, text="Network")
-network_label.grid(row=9, column=0, sticky="w")
-network_var = tk.StringVar(value=CACHE.get("network", default_args["network"]))
-network_choices = CONFIG["training_types"]["networks"].keys()
-network_option = tk.OptionMenu(root, network_var, *network_choices)
-network_option.grid(row=9, column=1, sticky="w")
-
-# optimizer
-optimizer_label = tk.Label(root, text="Optimizer")
-optimizer_label.grid(row=10, column=0, sticky="w")
-optimizer_var = tk.StringVar(value=CACHE.get("optimizer", default_args["optimizer"]))
-optimizer_choices = CONFIG["training_types"]["optimizers"].keys()
-optimizer_option = tk.OptionMenu(root, optimizer_var, *optimizer_choices)
-optimizer_option.grid(row=10, column=1, sticky="w")
-
-# identifier
-identifier_label = tk.Label(root, text="Identifier")
-identifier_label.grid(row=11, column=0, sticky="w")
-identifier_var = tk.StringVar(value="")
-identifier_entry = tk.Entry(root, textvariable=identifier_var)
-identifier_entry.grid(row=11, column=1)
-
-# class
-class_label = tk.Label(root, text="Class")
-class_label.grid(row=12, column=0, sticky="w")
-class_var = tk.StringVar(value="")
-class_entry = tk.Entry(root, textvariable=class_var)
-class_entry.grid(row=12, column=1)
-
-# regularization images count
-reg_count_label = tk.Label(root, text="Regularization images count")
-reg_count_label.grid(row=13, column=0, sticky="w")
-reg_count_var = tk.IntVar(value=0)
-reg_count_entry = tk.Entry(root, textvariable=reg_count_var)
-reg_count_entry.grid(row=13, column=1)
-
-# augmentation
-augmentation_label = tk.Label(root, text="Augmentation")
-augmentation_label.grid(row=14, column=0, sticky="w")
-
-fliplr_label = tk.Label(root, text="fliplr")
-fliplr_label.grid(row=15, column=0, sticky="w")
-fliplr_var = tk.BooleanVar(value=default_args["fliplr"])
-fliplr_checkbox = tk.Checkbutton(root, variable=fliplr_var)
-fliplr_checkbox.grid(row=15, column=1, sticky="w")
-
-coloraug_label = tk.Label(root, text="color_aug")
-coloraug_label.grid(row=16, column=0, sticky="w")
-coloraug_var = tk.BooleanVar(value=default_args["color_aug"])
-coloraug_checkbox = tk.Checkbutton(root, variable=coloraug_var)
-coloraug_checkbox.grid(row=16, column=1, sticky="w")
-
-randomcrop_label = tk.Label(root, text="random_crop")
-randomcrop_label.grid(row=17, column=0, sticky="w")
-randomcrop_var = tk.BooleanVar(value=default_args["random_crop"])
-randomcrop_checkbox = tk.Checkbutton(root, variable=randomcrop_var)
-randomcrop_checkbox.grid(row=17, column=1, sticky="w")
-
-catptiondropoutrate_label = tk.Label(root, text="caption_dropout_rate")
-catptiondropoutrate_label.grid(row=18, column=0, sticky="w")
-catptiondropoutrate_var = tk.DoubleVar(value=default_args["caption_dropout_rate"])
-catptiondropoutrate_entry = tk.Entry(root, textvariable=catptiondropoutrate_var)
-catptiondropoutrate_entry.grid(row=18, column=1)
-
-
+add_entry("Model Name")
+add_checkbox("Is SDXL")
+add_path_entry("Image Source Directory", dir=True)
+add_path_entry("StableDiffusion Model Path")
+add_path_entry("VAE Path")
+add_label("Training Options")
+add_entry("Steps", "int", 6000)
+add_entry("Batch Size", "int", 4)
+add_entry("Learning Rate", "double", 1e-4)
+add_checkbox("Train U-Net Only")
+add_checkbox("Enable image ratio bucket")
+add_checkbox("Cache latents")
+add_checkbox("xformers")
+add_option_menu("Network", CONFIG["training_types"]["networks"].keys())
+add_option_menu("Optimizer", CONFIG["training_types"]["optimizers"].keys())
+add_entry("Caption Dropout Rate", "double")
+add_checkbox("Suffle Caption")
+add_option_menu("Network", CONFIG["training_types"]["networks"].keys())
+add_entry("Identifier")
+add_entry("Class")
+add_entry("Regularization images count", "int", -1)
+add_label("Augmentation")
+add_checkbox("Flip LR")
+add_checkbox("Color Aug")
+add_checkbox("Random Crop")
+add_label("Sample output")
+add_option_menu("sampler", CONFIG["sample"]["samplers"])
 
 def get_values():
-    values = {
-        "model_name": model_output_name_entry.get(),
-        "src": src_entry.get(),
-        "sd_path": sd_path_entry.get(),
-        "steps": steps_var.get(),
-        "batch_size": batch_size_var.get(),
-        "learning_rate": learning_rate_var.get(),
-        "enable_bucket": enable_bucket_var.get(),
-        "xformers": xformers_var.get(),
-        "network": network_var.get(),
-        "optimizer": optimizer_var.get(),
-        "identifier": identifier_var.get(),
-        "class": class_var.get(),
-        "reg_count": reg_count_var.get(),
-        "fliplr": fliplr_var.get(),
-        "color_aug": coloraug_var.get(),
-        "random_crop": randomcrop_var.get(),
-        "caption_dropout_rate": catptiondropoutrate_var.get(),
-    }
-    values["dst"] = os.path.join(CONFIG["output_dir"], values["model_name"])
+    values = {k: v.get() for k, v in tk_values.items()}
+    values["dst"] = os.path.join(CONFIG["output_dir"], values["Model Name"])
     values["train_img_dst"] = os.path.join(values["dst"], "train")
     values["reg_img_dst"] = os.path.join(values["dst"], "reg")
     values["toml_path"] = os.path.join(values["dst"], "config.toml")
     values["sample_prompt_path"] = os.path.join(values["dst"], "prompt.txt")
+    if values["Model Name"] == "":
+        raise ValueError("Model Name cannot be empty")
+    if values["Regularization images count"] == "":
+        values["Regularization images count"] = -1
+    
 
-    if values["reg_count"] < 0:
-        values["reg_count"] = len(tagimg.find_images(values["src"]))
+    if int(values["Regularization images count"]) < 0:
+        values["Regularization images count"] = len(tagimg.find_images(values["Image Source Directory"]))
     return values
 
 def save_cache(values):
-    old = cache.load_cache()
-    cacheable = [
-        "sd_path",
-        "network",
-        "optimizer",
-        "xformers",
-        "steps",
-        "batch_size",
-        "learning_rate",
-    ]
-    for key in cacheable:
-        if old.get(key, None) != values[key]:
-            old[key] = values[key]
-    cache.save_cache(old)
+    data = {}
+    uncacheable_keys = ["dst", "train_img_dst", "reg_img_dst", "toml_path", "sample_prompt_path", "Model Name", "Image Source Directory"]
+    for key in values.keys():
+        if key not in uncacheable_keys:
+            data[key] = values[key]
+    cache.save_cache(data)
 
 def run():
     values = get_values()
     save_cache(values)
+
+    # make dst directory
     if os.path.exists(values["dst"]):
         raise FileExistsError(f"{values['dst']} already exists")
     os.makedirs(values["dst"], exist_ok=False)
 
     # setup datasets
-    tagimg.tagimg(values["src"], values["train_img_dst"])
-    if values["reg_count"] > 0:
+    tagimg.tagimg(values["Image Source Directory"], values["train_img_dst"])
+    if values["Regularization images count"] > 0:
         os.makedirs(values["reg_img_dst"], exist_ok=True)
-        if values["class"] == "":
-            generate_image.transparent_img(values["reg_img_dst"], values["reg_count"])
+        if values["Class"] == "":
+            if (values["Is SDXL"]):
+                generate_image.transparent_img(values["reg_img_dst"], values["Regularization images count"], 1024, 1024)
+            else:
+                generate_image.transparent_img(values["reg_img_dst"], values["Regularization images count"])
         else:
-            generate_image.txt2img(
-                model_path=values["sd_path"],
-                prompt=f"""{values['class']}, {CONFIG["generator"]["default_prompt"]}""",
-                negative_prompt=CONFIG["generator"]["default_negative_prompt"],
-                dst=values["reg_img_dst"],
-                count=values["reg_count"],
-            )
+            if (values["Is SDXL"]):
+                generate_image.txt2img_sdxl(
+                    model_path=values["StableDiffusion Model Path"],
+                    vae_path=values["VAE Path"],
+                    sampler=values["sampler"],
+                    prompt=f"""{values['Class']}, {CONFIG["generator"]["default_prompt"]}""",
+                    negative_prompt=CONFIG["generator"]["default_negative_prompt"],
+                    dst=values["reg_img_dst"],
+                    count=values["Regularization images count"],
+                )
+            else:
+                generate_image.txt2img(
+                    model_path=values["StableDiffusion Model Path"],
+                    vae_path=values["VAE Path"],
+                    sampler=values["sampler"],
+                    prompt=f"""{values['Class']}, {CONFIG["generator"]["default_prompt"]}""",
+                    negative_prompt=CONFIG["generator"]["default_negative_prompt"],
+                    dst=values["reg_img_dst"],
+                    count=values["Regularization images count"],
+                )
     
     # generate config.toml
     with open(os.path.join(BASE_DIR, "templates", "config.toml.jinja2"), "r") as f:
@@ -248,8 +226,8 @@ def run():
         f.write(script)
     
     # generate prompt for generate sample
-    if values["identifier"] != "":
-        prompt = f"""{CONFIG["sample"]["default_prompt"]}, {values["identifier"]} {values["class"]} --n {CONFIG["sample"]["default_negative_prompt"]}"""
+    if values["Identifier"] != "":
+        prompt = f"""{values["Identifier"]} {values["Class"]}, {CONFIG["sample"]["default_prompt"]} --n {CONFIG["sample"]["default_negative_prompt"]}"""
     else:
         caption_file_path = list(glob.glob(os.path.join(values["train_img_dst"], "*.txt")))[0]
         with open(caption_file_path, "r") as f:
@@ -265,7 +243,8 @@ def run():
     root.destroy()
 
 # run button
+next_row()
 run_button = ttk.Button(root, text="Run", command=run, style="TButton")
-run_button.grid(row=19, column=0, columnspan=3, sticky="e")
+run_button.grid(row=row, column=0, columnspan=3, sticky="e")
 
 root.mainloop()
--- utils/generate_image.py
+++ utils/generate_image.py
@@ -3,6 +3,7 @@ from PIL import Image
 from utils.config import CONFIG
 
 GEN_IMG_PATH = os.path.join(CONFIG["sd_scripts_path"], "gen_img_diffusers.py")
+GEN_IMG_SDXL_PATH = os.path.join(CONFIG["sd_scripts_path"], "sdxl_gen_img.py")
 
 def transparent_img(dir, n, h=512, w=512):
     # generate n transparent images
@@ -11,7 +12,7 @@ def transparent_img(dir, n, h=512, w=512):
         img = Image.new('RGBA', (w, h), (0, 0, 0, 0))
         img.save(path)
 
-def txt2img(model_path, prompt, negative_prompt, dst, count=1):
+def txt2img(model_path, vae_path, sampler, prompt, negative_prompt, dst, count=1):
     # exec shell command
     abs_model_path = os.path.abspath(model_path)
     abs_dst = os.path.abspath(dst)
@@ -20,10 +21,35 @@ def txt2img(model_path, prompt, negative_prompt, dst, count=1):
     command = f'''python "{GEN_IMG_PATH}"
         --ckpt "{abs_model_path}"
         --outdir "{abs_dst}"
+        --vae "{os.path.abspath(vae_path)}"
+        --no_half_vae
         --images_per_prompt {count}
         --batch_size {CONFIG["generator"]["batch_size"]}
         --steps {CONFIG["generator"]["steps"]}
-        --sampler {CONFIG["generator"]["sampler"]}
+        --sampler {sampler}
         --prompt "{prompt} {f"--n {negative_prompt}" if negative_prompt else ""}"
     '''
+    print((" ").join([x.strip() for x in command.splitlines()]))
+    os.system((" ").join([x.strip() for x in command.splitlines()]))
+
+def txt2img_sdxl(model_path, vae_path, sampler, prompt, negative_prompt, dst, count=1):
+    # exec shell command
+    abs_model_path = os.path.abspath(model_path)
+    abs_dst = os.path.abspath(dst)
+    os.makedirs(abs_dst, exist_ok=True)
+    os.chdir(CONFIG["sd_scripts_path"])
+    command = f'''python "{GEN_IMG_SDXL_PATH}"
+        --ckpt "{abs_model_path}"
+        --no_half_vae
+        --vae "{os.path.abspath(vae_path)}"
+        --outdir "{abs_dst}"
+        --images_per_prompt {count}
+        --batch_size {CONFIG["generator"]["batch_size"]}
+        --steps {CONFIG["generator"]["steps"]}
+        --sampler {sampler}
+        --prompt "{prompt} {f"--n {negative_prompt}" if negative_prompt else ""}"
+        --H 1024
+        --W 1024
+    '''
+    print((" ").join([x.strip() for x in command.splitlines()]))
     os.system((" ").join([x.strip() for x in command.splitlines()]))
